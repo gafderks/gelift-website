f0eef8a6e2e5d91f0cdc44e7e44b7c3aee1820f4
Geert Derks <g.a.f.derks@student.tue.nl>
2017-11-14 22:54:43 +0100

    Added score list

--- winners.php
+++ winners.php
@@ -3,11 +3,20 @@
     <head>
         <meta charset="utf-8" />
         <title>GELIFT - Winners</title>
+        
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
         <link href="http://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
+        
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.css"/>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+ 
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.16/datatables.min.js"></script>
+
+
 		
         <!--[if IE]>
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -29,6 +38,18 @@
 			article {
 				margin-bottom: 100px;
 			}
+            
+            .scorelist .dq {
+                background-color: #F4CCCC;
+            }
+            
+            .scorelist .fa-check-circle-o {
+                color: green;
+            }
+            
+            .scorelist .fa-times {
+                color: red;
+            }
 		</style>
     </head>
     <body class="">
@@ -39,34 +60,209 @@
 			
 			<article>
 				<h1>Copenhagen 2017<small>Winners</small></h1>
+                <p><em>10-12 November</em></p>
                 
                 <table style="width: 100%; table-layout:fixed;">
                     <tbody>
 						<tr>
 							<td colspan="3">
-								<img src="http://via.placeholder.com/720x540?text=To+be+revealed..." class="img-fluid winner-photo" alt="???">
+								<img src="photo/winner/2017.jpg" class="img-fluid winner-photo" alt="Laura and Levi">
 							</td>
 						</tr>
                         <tr>
                             <td style="text-align: right; vertical-align: middle;">
-								<a href="https://gewis.nl/member/8091" target="_blank" title="s profile">
-									<span class="winner-firstname">???</span>
-									<span class="winner-surname"></span>
+								<a href="https://gewis.nl/member/7226" target="_blank" title="Laura's profile">
+									<span class="winner-firstname">Laura</span>
+									<span class="winner-surname">Kuntze</span>
 								</a>
                             </td>
                             <td style="width: 100px; text-align: center;">
                                 <img src="img/trophy.svg" width="42" height="42" alt="Trophy" /><br />
-                                <strong>? h ? min</strong>
+                                <strong>13 h 33 min</strong>
                             </td>
                             <td style="text-align: left; vertical-align: middle;">
-								<a href="https://gewis.nl/member/8413" target="_blank" title="'s profile">
-									<span class="winner-firstname">???</span>
-									<span class="winner-surname"></span>
+								<a href="https://gewis.nl/member/7130" target="_blank" title="Levi's profile">
+									<span class="winner-firstname">Levi</span>
+									<span class="winner-surname">Prikken</span>
 								</a>
                             </td>
                         </tr>
                     </tbody>
-                </table>    
+                </table>
+
+                <h3>Score list</h3>
+                <table class="scorelist table table-sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Team members</th>
+                            <th>Depart <abbr title="Eindhoven">E</abbr>-<abbr title="Bremen">B</abbr></th>
+                            <th>Arrive <abbr title="Eindhoven">E</abbr>-<abbr title="Bremen">B</abbr></th>
+                            <th>Depart <abbr title="Bremen">B</abbr>-<abbr title="Copenhagen">C</abbr></th>
+                            <th>Arrive <abbr title="Bremen">B</abbr>-<abbr title="Copenhagen">C</abbr></th>
+                            <th>Bonus time</th>
+                            <th>Total time</th>
+                            <th><abbr title="Disqualified">DQ</abbr></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Laura &amp; Levi</td>
+                            <td>16:50</td>
+                            <td>23:10</td>
+                            <td>09:17</td>
+                            <td>16:50</td>
+                            <td>0:20</td>
+                            <td>13:33</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Ruben &amp; Rutger</td>
+                            <td>17:37</td>
+                            <td>23:37</td>
+                            <td>09:23</td>
+                            <td>18:37</td>
+                            <td>1:10</td>
+                            <td>14:04</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Amarens &amp; Maureen</td>
+                            <td>10:06</td>
+                            <td>17:05</td>
+                            <td>09:24</td>
+                            <td>18:45</td>
+                            <td>0:00</td>
+                            <td>15:59</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Jelle &amp; Alessa</td>
+                            <td>16:01</td>
+                            <td>00:04</td>
+                            <td>09:36</td>
+                            <td>18:59</td>
+                            <td>0:00</td>
+                            <td>17:26</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>Anja &amp; Bor</td>
+                            <td>12:47</td>
+                            <td>22:26</td>
+                            <td>09:24</td>
+                            <td>19:51</td>
+                            <td>0:00</td>
+                            <td>20:06</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>Sven &amp; Celine</td>
+                            <td>12:25</td>
+                            <td>19:28</td>
+                            <td>09:28</td>
+                            <td>23:30</td>
+                            <td>0:00</td>
+                            <td>21:05</td>
+                            <td><i class="fa fa-check-circle-o" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>Amber &amp; Robin</td>
+                            <td class="dq">16:12</td>
+                            <td class="dq">21:22</td>
+                            <td>09:24</td>
+                            <td>18:45</td>
+                            <td>0:00</td>
+                            <td>14:31</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>Paco &amp; Koen</td>
+                            <td class="dq">17:19</td>
+                            <td class="dq">04:00</td>
+                            <td>09:29</td>
+                            <td>18:19</td>
+                            <td>0:00</td>
+                            <td>19:31</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Evelien &amp; Steven</td>
+                            <td>18:15</td>
+                            <td>01:12</td>
+                            <td class="dq">09:55</td>
+                            <td class="dq">22:30</td>
+                            <td>0:00</td>
+                            <td>19:32</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>Arend &amp; Robert</td>
+                            <td class="dq">15:58</td>
+                            <td class="dq">23:40</td>
+                            <td class="dq">09:36</td>
+                            <td class="dq">23:13</td>
+                            <td>0:50</td>
+                            <td>20:29</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>Thijs &amp; Lars</td>
+                            <td>16:10</td>
+                            <td>00:38</td>
+                            <td class="dq">09:23</td>
+                            <td class="dq">23:04</td>
+                            <td>0:20</td>
+                            <td>21:49</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>Jochem &amp; Yorick</td>
+                            <td class="dq">14:23</td>
+                            <td class="dq">04:00</td>
+                            <td>09:30</td>
+                            <td>20:45</td>
+                            <td>0:40</td>
+                            <td>24:32</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>13</td>
+                            <td>Geert &amp; Guido</td>
+                            <td>12:39</td>
+                            <td>01:04</td>
+                            <td class="dq">10:02</td>
+                            <td class="dq">06:49</td>
+                            <td>0:40</td>
+                            <td>32:32</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                        <tr>
+                            <td>14</td>
+                            <td>Armand &amp; Remco</td>
+                            <td class="dq">10:53</td>
+                            <td class="dq">04:00</td>
+                            <td class="dq">10:07</td>
+                            <td class="dq">13:46</td>
+                            <td>0:30</td>
+                            <td>44:16</td>
+                            <td><i class="fa fa-times" aria-hidden="true"></i></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <small>Red shading means that this team was disqualified for the shaded section.<small>
 				</article>
 			
 			
@@ -77,7 +273,7 @@
                     <tbody>
 						<tr>
 							<td colspan="3">
-								<img src="photo/winner/2016.jpeg" class="img-fluid winner-photo" alt="Bart and Ruben">
+								<img src="photo/winner/2016.jpg" class="img-fluid winner-photo" alt="Bart and Ruben">
 							</td>
 						</tr>
                         <tr>
@@ -170,6 +366,16 @@
 				
             </section>
         </div>
+        
+        <script type="application/javascript">
+        $(document).ready( function () {
+            $('.scorelist').DataTable({
+                paging: false,
+                info: false,
+                dom: "t"
+            });
+        } );
+        </script>
         
     </body>
 </html>
